503351a65930ee4049db5cdd232d503b152542b4
pyrex41 <me@notreal.com>
2020-03-24 16:49:22 -0400

    add CORS

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_cors import CORS
 from config import Config
 
-app = Flask(__name__)
+app = CORS(Flask(__name__))
 app.config.from_object(Config)
 
 from app import routes
