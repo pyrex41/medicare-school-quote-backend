c72c61ab90055c8749844c4bbe8734a2ba48d75c
pyrex41 <reub.brooks@gmail.com>
2022-09-24 13:27:46 -0700

    roll back

--- csg.py
+++ csg.py
@@ -167,16 +167,6 @@ class csgRequest:
                             d.append((kk, rate, naic))
                     else:
                         d.append((kk, rate, naic))
-            """
-            elif naic == '61727': # workaround for CIGNA standard II -- new naic
-                if 'standard' in kk.lower():
-                    naic = naic + '002'
-                    if has_h:
-                        if bool(household) == has_h:
-                            d.append((kk, rate, naic))
-                    else:
-                        d.append((kk, rate, naic))
-            """
             else:
                 if has_h: # workaround for Humana // Household
                     if bool(household) == has_h:
